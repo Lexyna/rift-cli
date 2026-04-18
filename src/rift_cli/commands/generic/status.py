ce960bee915874ff0cb0390255f9d97cc2e72473
Alexandra Thubeauville <alexandra.thubeauville@digits.schwarz>
2026-04-18 11:15:18 +0200

    Moved tick logic to main/cli command

--- src/rift_cli/commands/generic/status.py
+++ src/rift_cli/commands/generic/status.py
@@ -19,25 +19,13 @@ def status() -> None:
     if game.options.paused:
         console.log(Text(f"Paused"))
         print_resources(game)
-        game.last_update = curr_time
+        game.last_update = time.time()
         save_game(game)
         return
 
     msg_status = Text(f"Ticks since game started: ")
     msg_status.append(f"{game.current_tick}", f"bold {color.green}")
     console.log(msg_status)
-
-    curr_time: float = time.time()
-    delta_time: float = curr_time - game.last_update
-
-    compounded_ticks: int = floor(delta_time/game.options.tickrate)
-    console.log(f"{compounded_ticks} tick passed since last update")
-
-    if compounded_ticks > 0:    
-        tick(game, compounded_ticks)
-        game.current_tick += compounded_ticks
-        game.last_update = curr_time
-        save_game(game)
 
     print_resources(game)
 
--- src/rift_cli/main.py
+++ src/rift_cli/main.py
@@ -1,11 +1,30 @@
+from math import floor
+import time
+
 import click
 from rift_cli.commands.buildings.building import buildings
 from rift_cli.commands.generic.init import init
 from rift_cli.commands.generic.status import status
 from rift_cli.commands.planets.planets import planets
+from rift_cli.data.game.gamedata import GameData
+from rift_cli.display.console import console
+from rift_cli.functions.generic.game import load_game, save_game, tick
 
 @click.group
 def cli():
+    game: GameData = load_game()
+    
+    curr_time: float = time.time()
+    delta_time: float = curr_time - game.last_update
+
+    compounded_ticks: int = floor(delta_time/game.options.tickrate)
+    console.log(f"{compounded_ticks} tick passed since last update")
+
+    if compounded_ticks > 0 and not game.options.paused:    
+        tick(game, compounded_ticks)
+        game.current_tick += compounded_ticks
+        game.last_update = curr_time
+        save_game(game)
     pass
 
 cli.add_command(init)
