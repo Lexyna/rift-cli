5dd7528c761044cfa9033a436d10fbe44fc92be8
Alexandra Thubeauville <alexandra.thubeauville@digits.schwarz>
2026-04-19 19:04:16 +0200

    Added deposit print to planets/tree command

--- src/rift_cli/commands/planets/tree.py
+++ src/rift_cli/commands/planets/tree.py
@@ -4,6 +4,7 @@ from rich.columns import Columns
 from rift_cli.data.game.gamedata import GameData, game_ctx
 import click
 
+from rift_cli.data.resources import print_resource
 from rift_cli.display.console import console
 
 @click.command
@@ -14,10 +15,17 @@ def tree(game: GameData) -> None:
 
     for p in game.planets.values():
         planet_tree = Tree(p.name + f" ({len(p.slots)}/{p.max_slots})")
+
+        building_branch = planet_tree.add("buildings")
         for id in p.slots:
             if id in game.buildings:
-                planet_tree.add(game.buildings[id].name + f" (lv.{game.buildings[id].level})")
+                building_branch.add(game.buildings[id].name + f" (lv.{game.buildings[id].level})")
         tree_list.append(planet_tree)
+
+        resource_brnach = planet_tree.add("Resources")
+        for value in p.deposits:
+            resource_brnach.add(print_resource(value, p.deposits[value]))
+
     
     coloumns = Columns(tree_list)
     console.log(coloumns)
--- src/rift_cli/data/resources.py
+++ src/rift_cli/data/resources.py
@@ -1,3 +1,7 @@
+from rich.text import Text
+
+from rift_cli.display.formatter import format_number
+from rift_cli.utils.colors import color
 from dataclasses import dataclass
 from enum import Enum
 
@@ -10,3 +14,13 @@ class Deposit:
     type: ResourceType
     amount: int
 
+def print_resource(type: ResourceType, value: int) -> str:
+    
+    txt_color = ""
+
+    match type:
+        case ResourceType.METAL: txt_color = f"{color.black}"
+        case ResourceType.CREDITS: txt_color = f"{color.cyan}"
+        case _: txt_color = f"{color.white}" 
+
+    return Text(f"{type.name}: {format_number(value)}", f"{txt_color}")
