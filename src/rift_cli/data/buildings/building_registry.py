2172c3e197670e6c13625fce4482da0028d8aae0
Alexandra Thubeauville <alexandra.thubeauville@digits.schwarz>
2026-04-20 09:39:50 +0200

    Added building registry for create/destroy

--- src/rift_cli/data/buildings/building_registry.py
+++ src/rift_cli/data/buildings/building_registry.py
@@ -5,13 +5,33 @@ from rich.progress import Progress, Task, TaskID
 from rift_cli.data.buildings.building import Building
 from rift_cli.data.game.gamedata import GameData
 
+BuildingCreate = Callable[[Building, GameData], None]
+BuildingDestroy = Callable[[Building, GameData], None]
+
 BuildingTick = Callable[[Building, GameData, int], None]
 BuildingDisplay = Callable[[Building, GameData, int], None]
 BuildingDisplayLive = Callable[[Progress, Building, GameData, int], TaskID]
 
 registry_building: dict[str, BuildingTick] = {}
+
+registry_building_create: dict[str, BuildingCreate] = {}
+registry_building_destroy: dict[str, BuildingCreate] = {}
+
 registry_display_building: dict[str, BuildingDisplay] = {}
 registry_display_live_building: dict[str, BuildingDisplayLive] = {}
+
+def building_create(name: str):
+    def decorator(fn: BuildingCreate) -> BuildingCreate:
+        registry_building_create[name] = fn
+        return fn
+    pass
+
+def building_destroy(name: str):
+    def decorator(fn: BuildingDestroy) -> BuildingDestroy:
+        registry_building_destroy[name] = fn
+        return fn
+    pass
+
 
 def building_tick(name: str):
     def decorator(fn: BuildingTick) -> BuildingTick:
