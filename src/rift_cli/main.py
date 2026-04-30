1143106e43aabfda1b3fa0ee36d105e612d388aa
Alexandra Thubeauville <alexandra.thubeauville@digits.schwarz>
2026-04-30 21:07:10 +0200

    Added time info  to last update tick message

--- src/rift_cli/main.py
+++ src/rift_cli/main.py
@@ -1,3 +1,4 @@
+import datetime
 from math import floor
 import os
 import time
@@ -11,6 +12,7 @@ from rift_cli.commands.generic.test import test
 from rift_cli.commands.planets.planets import planets
 from rift_cli.data.game.gamedata import GameData
 from rift_cli.display.console import console
+from rift_cli.functions.generic import game
 from rift_cli.functions.generic.game import load_game, save_game, tick
 from rift_cli.utils.vars import RIFT_FOLDER
 
@@ -31,7 +33,9 @@ def cli(ctx: click.Context):
     delta_time: float = curr_time - game_ctx.last_update
 
     compounded_ticks: int = floor(delta_time/game_ctx.options.tickrate)
-    console.log(f"{compounded_ticks} tick passed since last update")
+    time_string = datetime.timedelta(seconds=compounded_ticks*game_ctx.options.tickrate)
+
+    console.log(f"{compounded_ticks} ({time_string}) tick passed since last update")
 
     if compounded_ticks > 0 and not game_ctx.options.paused:    
         tick(game_ctx, compounded_ticks)
